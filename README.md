4a9bee92a1e23cecbbaf6c3379778cea0a957e54
ArnauFerre <101815929+ArnauFerre@users.noreply.github.com>
2022-03-28 16:43:29 +0200

    Update and rename README.md to nom_java.java

--- README.md
+++
@@ -1,1 +0,0 @@
-# arnau_java
---
+++ nom_java.java
@@ -0,0 +1,10 @@
+# arnau_java
+public class nom_java {
+	
+	public static void main(String[] args) {
+		
+		String XXX ="Arnau Ferre Reyes";
+		System.out.println("Hola Jenkins, sóc "+XXX);
+	}
+
+}
